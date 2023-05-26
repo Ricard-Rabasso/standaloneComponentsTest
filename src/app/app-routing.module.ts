bfac6b4284635dad6ccfbb8032682234ba55173a
Ricard-Rabasso <ricard@rabasso.es>
2023-05-26 08:30:22 +0200

    created the first standalone component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
   { path: 'cats', loadChildren: () => import('./pages/cats/cats.module').then(m => m.CatsModule) },
   { path: 'dogs', loadChildren: () => import('./pages/dogs/dogs.module').then(m => m.DogsModule) },
+  { path: 'first', loadComponent: () => import('./pages/my-first-standalone/my-first-standalone.component')},
 ];
 
 @NgModule({
